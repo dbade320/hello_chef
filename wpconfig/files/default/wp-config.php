c7266da60fb98dc5f004742729e95b33d181caff
dbade320 <dbade320@yahoo.in>
2016-08-05 11:10:49 +0530

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'wordpress.dbuclp.0001.aps1.cache.amazonaws.com');
  
-define('WP_HOME','wordpress-790172924.ap-south-1.elb.amazonaws.com');
-define('WP_SITEURL','wordpress-790172924.ap-south-1.elb.amazonaws.com');
+define('WP_HOME', 'wordpress-1634645544.ap-south-1.elb.amazonaws.com');
+define('WP_SITEURL', 'wordpress-1634645544.ap-south-1.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
